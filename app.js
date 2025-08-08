5e5d6fd40e3140950cb5565a77438cc47bcaa291
amagai-taimeishoji <amagai.taimeishoji@gmail.com>
2025-08-09 06:37:17 +0900

    app.js を更新
    うーむ

--- app.js
+++ app.js
@@ -185,11 +185,11 @@ document.getElementById("search-button").addEventListener("click", async () => {
     createTable("ranking-table", [
       ["累計半荘数\nランキング", "総スコア\nランキング", "最高スコア\nランキング", "平均スコア\nランキング", "平均着順\nランキング"],
       [
-        `${data["累計半荘数ランキング"]}位`,
-        `${data["総スコアランキング"]}位`,
-        `${data["最高スコアランキング"]}位`,
-        `${data["平均スコアランキング"]}位`,
-        `${data["平均着順ランキング"])}位`
+        format(data["累計半荘数ランキング"]),
+         format(data["総スコアランキング"]),
+         format(data["最高スコアランキング"]),
+         format(data["平均スコアランキング"]),
+         format(data["平均着順ランキング"])
       ]
     ], 5);
 
@@ -198,8 +198,8 @@ document.getElementById("search-button").addEventListener("click", async () => {
       ["累計半荘数", "総スコア", "最高スコア", "平均スコア", "平均着順"],
       [
         `${Number(data["累計半荘数"]).toFixed(0)}半荘`,
-        `${Number(data["総スコア"]).toFixed(3)}pt`,
-        `${Number(data["最高スコア"]).toFixed(3)}pt`,
+        `${Number(data["総スコア"]).toFixed(1)}pt`,
+        `${Number(data["最高スコア"]).toFixed(1)}pt`,
         `${Number(data["平均スコア"]).toFixed(3)}pt`,
         `${Number(data["平均着順"]).toFixed(3)}位`
       ]
